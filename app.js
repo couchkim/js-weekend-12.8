5adc0d6accc73ba4ac6e4a1bf40bbebdde295a81
Kim Hanson <couchkim3800@gmail.com>
2016-12-08 12:51:22 -0500

    Problem 2

--- app.js
+++ app.js
@@ -10,3 +10,24 @@ let total2=sum(7430, 2354.98);
 
 console.log(total1);
 console.log(total2);
+
+
+
+
+console.log("Problem Two - Avg");
+
+function avg(num1, num2, num3){
+    return (num1 + num2 + num3)/3;
+}
+
+let test1 = avg(5, 10, 15);
+let test2 = avg(100, 200, 600);
+let test3 = avg(234324, 4324234, 234234);
+
+console.log(test1);
+console.log(test2);
+console.log(test3);
+
+
+
+
