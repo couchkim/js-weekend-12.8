6f88916f39168df3edcf678f806e65f28af8092a
Kim Hanson <couchkim3800@gmail.com>
2016-12-08 12:45:08 -0500

    Problem 1

---
+++ app.js
@@ -0,0 +1,12 @@
+
+console.log("Problem One - Sum");
+
+function sum(num1,num2){
+    return num1 + num2;
+}
+
+let total1=sum(5, 10);
+let total2=sum(7430, 2354.98);
+
+console.log(total1);
+console.log(total2);
